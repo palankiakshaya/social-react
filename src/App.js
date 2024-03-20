5e8df1b5302ef1aff7c8f952fc88a42466d419ac
akshaya <akshayapalanki@lyrostech.com>
2024-03-20 14:10:30 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import { useState, createContext, useContext } from "react";
-import Login from "./Login";
-import Register from "./Register";
-import Home from "./Home";
-export const UserContext = createContext();
+import React, {useContext}  from 'react'
+import { UserContext } from './components/UserContext/Usercontext'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from './components/home/Home'
+import Login from './components/user/Login'
+import Register from './components/user/Register'
+import Feeds from './components/feeds/Feeds';
+import Albums from './components/albums/Albums';
+import Users from './components/user/Users';
+import Todos from './components/todos/Todos';
+
+
+// We should call login comp and register 
 export default function App() {
-  const [user, setUser] = useState("John");
-  const [flag, setFlag] = useState(0);
- 
+    const  {flag, setFlag, name} =useContext(UserContext);
+
   return (
-    <>
-      <UserContext.Provider value={{ user, setUser, flag, setFlag } }>
-       
-        {flag==0||flag==1?<Login/>:<Home/>}
-     
-      </UserContext.Provider>
-    </>
-  );
+    <div>
+        <Router>
+        {flag == 0 || flag==2 ? <Login/>:<Home />}
+        {flag ===2 && <Register/>}
+        
+        <Routes>
+        <Route path="/feeds" element={<Feeds />}></Route>
+            <Route path="/albums" element={<Albums />}></Route>
+            <Route path="/user" element={<Users />}></Route>
+            <Route path="/todos" element={<Todos />}></Route>
+        </Routes>
+        </Router>
+        
+    </div>
+  )
 }
